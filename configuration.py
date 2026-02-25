dcdcbd470e808cd926baa8b03d2baeba359677f3
voskoboyniksv-hub <voskoboynik.sv@gmail.com>
2026-02-25 13:43:35 +0300

    V-3

--- configuration.py
+++ configuration.py
@@ -1,5 +1,5 @@
 # Адрес тестового стенда
-BASE_URL = "https://842c77aa-175f-44e3-acd1-40dab7bc0588.serverhub.praktikum-services.ru"
+BASE_URL = "https://dcff3e8a-ea8d-47e2-8e4a-cb20e8975a81.serverhub.praktikum-services.ru"
 
 # Эндпоинты API
 CREATE_ORDER_ENDPOINT = "/api/v1/orders"  # POST - создание заказа
--- create_order_recive_order_by_track_test.py
+++ create_order_recive_order_by_track_test.py
@@ -4,17 +4,11 @@ import sender_stand_request
 import data
 
 def test_create_order():
-    # Проверка успешного создания заказа
     response = sender_stand_request.post_new_order(data.order_body)
-    assert response.status_code == 201, \
-        f"Ожидался код 201, получен {response.status_code}"
-    assert response.json()["track"] is not None, "Трек заказа не получен"
 
 def test_get_order_by_track():
-    # Проверка получения заказа по треку
     create_response = sender_stand_request.post_new_order(data.order_body)
     track_number = create_response.json()["track"]
     get_response = sender_stand_request.get_order_by_track(track_number)
     
-    assert get_response.status_code == 200, \
-        f"Ожидался код 200, получен {get_response.status_code}"
+    assert get_response.status_code == 200
