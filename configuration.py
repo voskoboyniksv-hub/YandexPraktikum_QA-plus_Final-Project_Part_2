e1c85f34d7beb1092c06331b5fa25441a89cf49f
voskoboyniksv-hub <voskoboynik.sv@gmail.com>
2026-02-25 10:18:02 +0300

    V-2

--- configuration.py
+++ configuration.py
@@ -1,5 +1,5 @@
 # Адрес тестового стенда
-BASE_URL = "https://89e984e8-8559-4366-a5d9-45b926760b10.serverhub.praktikum-services.ru"
+BASE_URL = "https://842c77aa-175f-44e3-acd1-40dab7bc0588.serverhub.praktikum-services.ru"
 
 # Эндпоинты API
 CREATE_ORDER_ENDPOINT = "/api/v1/orders"  # POST - создание заказа
